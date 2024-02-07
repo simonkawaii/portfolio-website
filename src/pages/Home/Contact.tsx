627159d6e794bb00ec7dc626b3be813095465437
simonkawaii <skdevprogress@gmail.com>
2024-02-07 09:51:17 +0100

    style: Removed padding on X axis on inputs style: Added selection colors

--- src/pages/Home/Contact.tsx
+++ src/pages/Home/Contact.tsx
@@ -3,7 +3,7 @@ import { Button } from "../../components/Button/Button";
 const Contact = () => {
   return (
     <section className="bg-black border-y-[1px]  border-neutral-800 rounded-none flex   items-center justify-center flex-wrap p-8 py-[7rem]  ">
-      <div className="flex flex-col w-full gap-8 max-w-screen-xl h-full">
+      <div className="flex flex-col w-full gap-24 max-w-screen-xl h-full">
         <div>
           <h2>Contact</h2>
           <p className="text-secondary tracking-wide">Let's get linked in!</p>
@@ -14,36 +14,36 @@ const Contact = () => {
         >
           <div className="flex flex-col">
             <label
-              className="mb-4 border-[1px] border-transparent"
+              className="mb-4 text-neutral-400 border-[1px] border-transparent"
               htmlFor="contact-email"
             >
               Email
             </label>
             <input
-              className="px-4 py-8  appearance-none border-b border-neutral-800 rounded w-full  bg-transparent leading-tight focus:outline-none  focus:border-accent-opposite"
+              className=" py-4  appearance-none border-b border-neutral-800 rounded w-full  bg-transparent leading-tight focus:outline-none  focus:border-accent-opposite"
               id="contact-email"
               type="text"
             />
           </div>
           <div className="flex flex-col">
             <label
-              className="mb-4 border-[1px] border-transparent"
+              className="mb-4 text-neutral-400 border-[1px] border-transparent"
               htmlFor="contact-title"
             >
               Title
             </label>
             <input
-              className="px-4 py-8  appearance-none border-b border-neutral-800 rounded w-full  bg-transparent leading-tight focus:outline-none  focus:border-accent-opposite"
+              className=" py-4  appearance-none border-b border-neutral-800 rounded w-full  bg-transparent leading-tight focus:outline-none  focus:border-accent-opposite"
               id="contact-title"
               type="text"
             />
           </div>
           <div className="flex flex-col">
-            <label className="mb-4" htmlFor="contact-message">
+            <label className="mb-4 text-neutral-400 " htmlFor="contact-message">
               Message
             </label>
             <textarea
-              className="px-4 py-8  appearance-none border-b border-neutral-800 rounded w-full  bg-transparent leading-tight  
+              className=" py-4  appearance-none border-b border-neutral-800 rounded w-full  bg-transparent leading-tight  
               focus:outline-none  focus:border-accent-opposite"
               name="contact-message"
               id="contact-message"
